fe9473bc7cc1d059a88273d15ccdc8f8c7015dd1
fidansamet <fidansamet@hacettepe.edu.tr>
2019-12-07 20:47:48 +0300

    It needs a promise

--- public_html/app.js
+++ public_html/app.js
@@ -8,22 +8,12 @@
 // FIDAN SAMET 21727666
 // OGUZ BAKIR 21627007
 
-const triangleVertexNum = 12;
-const triangleFanNumber = 360;
-var ninjaStarData = [];
-var spinAngles = 0;
-var spinSpeed = 0;
-var startSpin = false;
-var startScale = false;
-var scaleBig = false;
-var scale = 0.25;
-var startSpiral = false;
-var spiralPos = 0.0;
-var spiralCounter = 0;
-var spiralAngle = 0;
-var numOfComponents = 2;        // x and y (2d)
+var teapotVertex = [];
+var teapotFragment = [];
+var numOfComponents = 3;        // x, y and z (3d)
 var offset = 0;
-var translationComp = 0.0;
+var normalize = false;
+var stride = 0;
 
 function main() {
     const canvas = document.querySelector("#glCanvas");
@@ -40,213 +30,55 @@ function main() {
     gl.depthFunc(gl.LEQUAL);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     var type = gl.FLOAT;
-    var normalize = false;
-    var stride = 0;
     
-    // DRAW TRIANGLES OF NINJA STAR
-    ninjaStarTriangle(); 
-    const triangleShader = initShaderProgram(gl, vertexShader, triangleFragmentShader);
-    const triangleBuffer = gl.createBuffer();
-    gl.enableVertexAttribArray(gl.getAttribLocation(triangleShader, 'i_position'));
-    var triangleSpinTheta = gl.getUniformLocation(triangleShader, 'u_spin_theta');
-    var triangleScale = gl.getUniformLocation(triangleShader, 'u_scale');
-    var triangleTranslateTheta = gl.getUniformLocation(triangleShader, 'u_translate_theta');
-    var triangleTranslationComp = gl.getUniformLocation(triangleShader, 'u_translation_comp');
+    readTeapotModelFile();
+    console.log(teapotVertex);
+    console.log(teapotFragment);
     
-    // DRAW CIRCLES OF NINJA STAR
-    ninjaStarCircle();
-    const circleShader = initShaderProgram(gl, vertexShader, circleFragmentShader);
-    const circleBuffer = gl.createBuffer();
-    gl.enableVertexAttribArray(gl.getAttribLocation(circleShader, 'i_position'));
-    var circleSpinTheta = gl.getUniformLocation(circleShader, 'u_spin_theta');
-    var circleScale = gl.getUniformLocation(circleShader, 'u_scale');
-    var circleTranslateTheta = gl.getUniformLocation(circleShader, 'u_translate_theta');
-    var circleTranslationComp = gl.getUniformLocation(circleShader, 'u_translation_comp');
+    const teapotShader = initShaderProgram(gl, vertexShader, fragmentShader);
+    const teapotBuffer = gl.createBuffer();
+    gl.enableVertexAttribArray(gl.getAttribLocation(teapotShader, 'i_position'));
     
     // ROTATION ANIMATION
     function render () {
-        
-        if (startSpin) {
-            // spinning
-            spinAngles += spinSpeed;
-        }
-        
-        if (startScale) {
-            // scaling
-            if (scale >= 0.375) {        // 1.5 scale
-                scaleBig = false;
-            } else if (scale <= 0.125) {     // 0.5 scale
-                scaleBig = true;
-            }
-            
-            if (scaleBig) {
-                scale += 0.0025;
-            } else {
-                scale -= 0.0025;
-            }
-        }
-        
-        if (startSpiral) {
-            // spiral rotating
-            
-            if ((spiralCounter < 0 && spiralAngle >= 450)) {
-                spiralAngle -= 900;
-            } else if ((spiralCounter > 0 && spiralAngle <= -450)) {
-                spiralAngle += 900;
-            }
-            spiralAngle -= spiralCounter;
-            console.log(spiralAngle);
-            translationComp = spiralAngle / -1080;
-        }
     
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         
         // TRIANGLES
-        gl.bindBuffer(gl.ARRAY_BUFFER, triangleBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(ninjaStarData), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, teapotBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(teapotVertex), gl.STATIC_DRAW);
     
         // TRIANGLE POSITIONS
         offset = 0;
-        gl.vertexAttribPointer(gl.getAttribLocation(triangleShader, 'i_position'),
+        gl.vertexAttribPointer(gl.getAttribLocation(teapotShader, 'i_position'),
             numOfComponents, type, normalize, stride, offset);
-        gl.useProgram(triangleShader);
-        gl.uniform1f(triangleSpinTheta, spinAngles);
-        gl.uniform1f(triangleScale, scale);
-        gl.uniform1f(triangleTranslateTheta, spiralAngle);
-        gl.uniform1f(triangleTranslationComp, translationComp);
+        gl.useProgram(teapotShader);
         
         // DRAW TRIANGLES
-        gl.drawArrays(gl.TRIANGLES, offset, triangleVertexNum);
-
-        // CIRCLES
-        gl.bindBuffer(gl.ARRAY_BUFFER, circleBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(ninjaStarData), gl.STATIC_DRAW);
+        console.log(teapotVertex.length);
+        gl.drawArrays(gl.TRIANGLES, offset, teapotVertex.length);
         
-        // CIRCLE POSITIONS
-        offset = (12 * 2) * 4;      // 2 components for positions, 3 for colors
-        gl.vertexAttribPointer(gl.getAttribLocation(circleShader, 'i_position'),
-                numOfComponents, type, normalize, stride, offset);
-        gl.useProgram(circleShader);
-        gl.uniform1f(circleSpinTheta, spinAngles);
-        gl.uniform1f(circleScale, scale);
-        gl.uniform1f(circleTranslateTheta, spiralAngle);
-        gl.uniform1f(circleTranslationComp, translationComp);
+        //requestAnimationFrame(render);
 
-        // DRAW CIRCLES
-        for (var i = 0; i < 5; i++) {
-            gl.drawArrays(gl.TRIANGLE_FAN, i * triangleFanNumber + i, triangleFanNumber);
-        }
-        
-        // animate on 2 and 3
-        if (startSpin || startScale || startSpiral) {
-            requestAnimationFrame(render);
-        }
     }
     
     render();
-    
-    document.getElementById("startSpin").onclick = function(){
-        if (startSpin == false) {
-            startSpin = true;
-            spinSpeed = parseInt(document.getElementById("spinCounter").value);
-            if (!(startSpiral || startScale)) { // if there is no animation going on
-                render();
-            }
-        }
-    };
-    document.getElementById("stopSpin").onclick = function(){
-        if (startSpin == true) {
-            startSpin = false;
-        }
-    };
-    document.getElementById("spinCounter").onclick = function(){
-        if (startSpin == true) {
-            spinSpeed = parseInt(document.getElementById("spinCounter").value);
-        }
-    };
-    document.getElementById("startScale").onclick = function(){
-        if (startScale == false) {
-            startScale = true;
-            scaleBig = true;
-            if (!(startSpin || startSpiral)) { // if there is no animation going on
-                render();
-            }
-        }
-    };
-    document.getElementById("stopScale").onclick = function(){
-        if (startScale == true) {
-            startScale = false;
-            scaleBig = false;
-        }
-    };
-    document.getElementById("startSpiral").onclick = function(){
-        if (startSpiral == false) {
-            startSpiral = true;
-            spiralCounter = parseInt(document.getElementById("spiralCounter").value);
-            if (!(startSpin || startScale)) { // if there is no animation going on
-                render();
-            }
-        }
-    };
-    document.getElementById("stopSpiral").onclick = function(){
-        if (startSpiral == true) {
-            startSpiral = false;
-        }
-    };
-    document.getElementById("spiralCounter").onclick = function(){
-        if (startSpiral == true) {
-            spiralCounter = parseInt(document.getElementById("spiralCounter").value);
-        }
-    };
 }
 
-function ninjaStarTriangle() {
-    var circlePositions = [
-        vec2(-1, 1),
-        vec2(1 / 3, 1 / 3),
-        vec2(-1 / 3, -1 / 3)
-    ];
-    ninjaStarData = ninjaStarData.concat(circlePositions[0], circlePositions[1], circlePositions[2]);
+function readTeapotModelFile () {
+    $.get('teapot.smf', function(data) {
+        var lines = data.split("\n");
 
-    var circlePositions = [
-        vec2(-1, -1),
-        vec2(-1 / 3, 1 / 3),
-        vec2(1 / 3, -1 / 3)
-    ];
-    ninjaStarData = ninjaStarData.concat(circlePositions[0], circlePositions[1], circlePositions[2]);
+        $.each(lines, function(n, elem) {
+            var res = elem.split(" ");
+            if (res[0] == "v") {
+                teapotVertex.push(res[1], res[2], res[3]);
+            } else {
+                teapotFragment.push(res[1], res[2], res[3]);
+            }
+        });
+    }, 'text');
 
-    var circlePositions = [
-        vec2(1, -1),
-        vec2(1 / 3, 1 / 3),
-        vec2(-1 / 3, -1 / 3)
-    ];
-    ninjaStarData = ninjaStarData.concat(circlePositions[0], circlePositions[1], circlePositions[2]);
-
-    var circlePositions = [
-        vec2(1, 1),
-        vec2(-1 / 3, 1 / 3),
-        vec2(1 / 3, -1 / 3)
-    ];
-    ninjaStarData = ninjaStarData.concat(circlePositions[0], circlePositions[1], circlePositions[2]);
-}
-
-function ninjaStarCircle() {
-    circle(0, 0);
-    circle(0, 1 / 2);
-    circle(-1 / 2, 0);
-    circle(0, -1 / 2);
-    circle(1 / 2, 0);
-}
-
-function circle(a, b) {
-    var origin = [a, b];
-    var r = 0.1;
-
-    for (var i = 0; i <= triangleFanNumber; i += 1) {
-        var j = i * Math.PI / 180;
-        var vert = [r * Math.sin(j) + a, r * Math.cos(j) + b];
-        ninjaStarData.push(vert[0], vert[1]);
-    }
 }
 
 main();
--- public_html/shader.js
+++ public_html/shader.js
@@ -6,52 +6,19 @@
 
 const vertexShader = `#version 300 es
 
-    in vec2 i_position;
-    uniform float u_translate_theta;
-    uniform float u_scale;
-    uniform float u_spin_theta;
-    uniform float u_translation_comp;
+    in vec3 i_position;
 
     void main() {
-        float spinAngles = radians(u_spin_theta);
-        float spinC = cos(spinAngles);
-        float spinS = sin(spinAngles);
-        float translateAngle = radians(u_translate_theta);
-        float translateC = cos(translateAngle);
-        float translateS = sin(translateAngle);
-        mat4 t = mat4(1.0, 0.0, 0.0, 0.0,
-                        0.0, 1.0, 0.0, 0.0,
-                        0.0, 0.0, 1.0, 0.0,
-                        u_translation_comp * translateC, u_translation_comp * translateS, 0.0, 1.0);
-        mat4 rz = mat4(spinC, spinS, 0.0, 0.0,
-                        -spinS, spinC, 0.0, 0.0,
-                        0.0, 0.0, 1.0, 0.0,
-                        0.0, 0.0, 0.0, 1.0);
-        mat4 s = mat4(u_scale, 0.0, 0.0, 0.0,
-                            0.0, u_scale, 0.0, 0.0,
-                            0.0, 0.0, u_scale, 0.0,
-                            0.0, 0.0, 0.0, 1.0);
-
-        gl_Position = t * rz * s * vec4(i_position, 0.0, 1.0);
+        gl_Position = vec4(i_position, 1.0);
     }
 `;
 
-const triangleFragmentShader = `#version 300 es
+const fragmentShader = `#version 300 es
 
     precision mediump float;
     out vec4 o_color;
     
     void main() {
         o_color = vec4(126.0 / 255.0, 126.0 / 255.0, 126.0 / 255.0, 1.0);
-    }
-`;
-
-const circleFragmentShader = `#version 300 es
-
-    precision mediump float;
-    out vec4 o_color;
-
-    void main() {
-        o_color = vec4(250.0 / 255.0, 237.0 / 255.0, 51.0 / 255.0, 1.0);
     }
 `;
